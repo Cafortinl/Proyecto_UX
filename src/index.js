c3292e41debf8245bc6ec7c78aa1086163d86730
Carlos Fortín <cafortinl@hotmail.com>
2021-12-19 23:33:04 -0600

    Second commit: updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { getAuth } from 'firebase/auth';
+import { FirebaseAppProvider, AuthProvider, useFirebaseApp } from 'reactfire';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkid5W9Od4W-yyMUd-8vANlv-qw-oLC7s",
+  authDomain: "proyectoux-11911015.firebaseapp.com",
+  projectId: "proyectoux-11911015",
+  storageBucket: "proyectoux-11911015.appspot.com",
+  messagingSenderId: "321865487132",
+  appId: "1:321865487132:web:97599d83bb4692f37920d9"
+};
+
+function FirebaseInitializedApp() {
+  const firestoreInstance = getFirestore(useFirebaseApp());
+  const firebaseAuth = getAuth(useFirebaseApp());
+
+  return (
+    <FirestoreProvider sdk={firestoreInstance}>
+      <AuthProvider sdk={firebaseAuth}>
+        <App />
+      </AuthProvider>
+    </FirestoreProvider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +34,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
